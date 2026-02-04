05646c605460ee55a1b223cd44839ec7e208bbf5
Administrator <ysrim@daum.net>
2026-02-04 13:57:54 +0900

    --

--- src/main/java/app/psn/stu/web/AttendanceController.java
+++ src/main/java/app/psn/stu/web/AttendanceController.java
@@ -1,22 +1,17 @@
 package app.psn.stu.web;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import app.psn.stu.service.AttendanceService;
 import com.base.annotation.MenuInfo;
 import com.base.enumm.MberGrdEnum;
 import com.base.enumm.NaviEnum;
 import com.base.utl.ResUtil;
 import com.base.utl.StringUtil;
-
-import app.psn.stu.service.AttendanceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
