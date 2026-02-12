5da19de065d1503d0eb35a4c74674e41b89ce731
ysrim <ysrimkr@gmail.com>
2026-02-12 20:11:01 +0900

    init

--- src/main/java/com/base/enumm/com/SessionKeyEnum.java
+++ src/main/java/com/base/enumm/com/SessionKeyEnum.java
@@ -7,12 +7,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SessionKeyEnum {
 
-	STD_MBER_INFO("mberInfo", "학생 사용자 정보 (StdSessionVO)"),         //
-	STD_MENU_INFO("menuInfo", "학생 접속 메뉴 정보 (String/MenuInfo)"),   //
-	TCH_MBER_INFO("mberInfo", "선생님 사용자 정보 (SessionVO)"),          //
-	TCH_MENU_INFO("menuInfo", "선생님 접속 메뉴 정보 (String/MenuInfo)"),  //
-	MNG_MBER_INFO("mberInfo", "관리자 사용자 정보 (SessionVO)"),           //
-	MNG_MENU_INFO("menuInfo", "관리자 접속 메뉴 정보 (String/MenuInfo)");  //
+	STD_MBER_INFO("stdMberInfo", "학생 사용자 정보 (StdSessionVO)"),         //
+	STD_MENU_INFO("stdMenuInfo", "학생 접속 메뉴 정보 (String/MenuInfo)"),   //
+	TCH_MBER_INFO("tchMberInfo", "선생님 사용자 정보 (SessionVO)"),          //
+	TCH_MENU_INFO("tchMenuInfo", "선생님 접속 메뉴 정보 (String/MenuInfo)"),  //
+	MNG_MBER_INFO("mngMberInfo", "관리자 사용자 정보 (SessionVO)"),           //
+	MNG_MENU_INFO("mngMenuInfo", "관리자 접속 메뉴 정보 (String/MenuInfo)");  //
 
 	private final String key;  // HttpSession에 저장될 실제 문자열 Key
 	private final String desc; // 무엇이 저장되는지 설명
