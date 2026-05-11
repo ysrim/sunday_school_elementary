6b211c30eb8d1c1579ca20529b62aa92e234388b
ysrim <ysrimkr@gmail.com>
2026-05-11 16:01:04 +0900

    to_do

--- src/main/java/app/psn/tch/qest/service/impl/TchQestServiceImpl.java
+++ src/main/java/app/psn/tch/qest/service/impl/TchQestServiceImpl.java
@@ -1,21 +1,23 @@
 package app.psn.tch.qest.service.impl;
 
+import java.util.List;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.base.enumm.com.QuestLogStatusEnum;
+import com.base.utl.SessionUtil;
+import com.base.vo.QuestCompleteEvent;
+
 import app.psn.com.service.DomainService;
 import app.psn.com.vo.QuestLogsVO;
 import app.psn.tch.qest.mapper.TchQestMapper;
 import app.psn.tch.qest.service.TchQestService;
 import app.psn.tch.qest.vo.ReqQestProcVO;
 import app.psn.tch.qest.vo.TchResQestVO;
-import com.base.enumm.com.QuestLogStatusEnum;
-import com.base.utl.SessionUtil;
-import com.base.vo.QuestCompleteEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Slf4j
 @Service("tchQuestService")
@@ -36,6 +38,8 @@ public class TchQestServiceImpl implements TchQestService {
 
 	@Override
 	public void questProc(ReqQestProcVO reqQestProcVO) {
+
+		// 퀘스트 승인처리는 요청한건에 대해서만 처리함. 리워드 중복 방지 로직 추가
 
 		// 1. 퀘스트 승인 처리
 		tchQestMapper.questProc(reqQestProcVO);
